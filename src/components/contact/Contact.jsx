3fb2c22fb5dfcb95e1140b08398fa76eaed22c04
Pyae Phyo <pyaephyo34@gmail.com>
2024-03-24 22:03:08 +0630

    update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -62,7 +62,7 @@ const Contact = () => {
           <button type="submit" className="btn btn-primary">
             Send Message
           </button>
-          {message && <span>Thanks, I'll reply ASAP :)</span>}
+          {message && <span>Thanks, I'll reply ASAP :</span>}
         </form>
       </div>
     </section>
